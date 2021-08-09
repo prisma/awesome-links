b53d9baab69aae04d1bbf2679ec54ddcde081a29
m-abdelwahab <hey@mahmoud.codes>
2021-08-09 14:42:44 +0200

    part-1 changes

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+import { links } from '../data/links';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: 'test@gmail.com',
+      role: 'ADMIN',
+    },
+  });
+
+  await prisma.link.createMany({
+    data: links,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect);
