4eb3b3f9332e9cf6e1815611963d47d26f9746f6
m-abdelwahab <hey@mahmoud.codes>
2021-09-04 23:50:59 +0200

    set up apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/apollo';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
