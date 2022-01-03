14ed5160d4dee4dc32af24dab279cbd805e297c0
m-abdelwahab <hey@mahmoud.codes>
2022-01-03 11:26:27 +0200

    add check for admin role

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { gql, useMutation } from '@apollo/client';
 import toast, { Toaster } from 'react-hot-toast';
+import { getSession } from '@auth0/nextjs-auth0';
+import { prisma } from '../lib/prisma';
 
 const CreateLinkMutation = gql`
   mutation (
@@ -127,3 +129,41 @@ const Admin = () => {
 };
 
 export default Admin;
+
+export const getServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/api/auth/login',
+      },
+      props: {},
+    };
+  }
+
+  const user = await prisma.user.findUnique({
+    select: {
+      email: true,
+      role: true,
+    },
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (user.role !== 'ADMIN') {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/404',
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
