4d29b2a013a15f1309a6871da2381bbc86300cab
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2022-03-29 22:25:48 +0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// /lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple instances of Prisma Client in development
-declare const global: NodeJS.Global & { prisma?: PrismaClient };
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
 
-export const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+let prisma: PrismaClient
 
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+export default prisma
