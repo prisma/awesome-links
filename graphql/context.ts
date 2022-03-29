1c7ed796002ba85e68cf43c22874a4c673624515
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2022-03-29 21:32:12 +0200

    update import

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { prisma } from '../lib/prisma';
+import prisma from '../lib/prisma';
 import { Claims, getSession } from '@auth0/nextjs-auth0';
 
 export type Context = {
