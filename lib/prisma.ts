87967895c1614358a2c74d118dfda001a6efec48
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2022-03-29 21:49:48 +0200

    update imports

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-// /lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
 // PrismaClient is attached to the `global` object in development to prevent
--- pages/api/auth/hook.ts
+++ pages/api/auth/hook.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../../../lib/prisma';
+import prisma from '../../../lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
